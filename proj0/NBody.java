92b37cae653d482d2b54fb66a3642278076b72fa
Jiaqi Huang <1404992421@qq.com>
2026-03-24 21:37:28 +0800

    project 0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -81,12 +81,13 @@ public class NBody {
             StdDraw.show();
             StdDraw.pause(10);
 
-            // to check whether it is correct
-            StdOut.printf("%d\n", planets.length);
-            StdOut.printf("%.2e\n", radius);
+
 
 
         }
+        // to check whether it is correct
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
         for (int i = 0; i < planets.length; i++) {
             StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
                     planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
