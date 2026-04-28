c29a2f579ebede8c9d914c269dc5a272854b0b3b
Jiaqi Huang <1404992421@qq.com>
2026-04-28 10:40:47 +0800

    2026.4.28 10:40

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,105 @@
+public class ArrayDeque<T> {
+
+    private T[] items;
+    private int size;
+    private int front;
+    private int rear;
+    private int arrayLength;
+
+    public static final int ARRAY_SIZE = 8;
+
+    // 构造函数
+    public ArrayDeque() {
+        items = (T[]) new Object[ARRAY_SIZE];
+        size = 0;
+        front = 0;
+        rear = 1;
+        arrayLength = ARRAY_SIZE;
+    }
+
+
+    // index计算工具
+    private int minusOne(int index) {
+        return (index - 1 + arrayLength) % arrayLength;
+    }
+    private int plusOne(int index) {
+        return (index + 1) % arrayLength;
+    }
+
+    // 更新数组的大小
+    private void resize(int capacity) {
+        T[] newItems = (T[]) new Object[capacity];
+
+        int index = plusOne(front);
+        for (int i = 0; i < size; i++) {
+            newItems[i] = items[(index + i) % arrayLength];
+        }
+        items = newItems;
+        arrayLength = capacity;
+        front = arrayLength - 1;
+        rear = size;
+
+    }
+
+    public void addFirst(T item) {
+        if (size + 1 == arrayLength) {
+            resize(arrayLength * 2);
+        }
+
+        front = minusOne(front);
+        items[front] = item;
+        size++;
+    }
+
+    public void addLast(T item) {
+        if (size + 1 == arrayLength) {
+            resize(arrayLength * 2);
+        }
+
+        items[rear] = item;
+        rear = plusOne(rear);
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        int tempIndex = front;
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[tempIndex] + " ");
+            tempIndex = plusOne(tempIndex);
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+
+        if ((size + 1) <= (arrayLength / 4)) {
+            resize(arrayLength / 2);
+        }
+
+        T tempValue = items[front];
+        front = plusOne(front);
+        size--;
+        return tempValue;
+    }
+
+    public T removeLast() {
+        if ((size + 1) <= (arrayLength / 4)) {
+            resize(arrayLength / 2);
+        }
+        rear = minusOne(rear);
+        size--;
+        return items[rear];
+    }
+
+    public T get(int index) {
+        return (items[(front + index) % arrayLength]);
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,126 @@
+import sun.awt.image.ImageWatched;
+
+public class LinkedListDeque<T> {
+    /* 首先定义链表的结构
+     */
+    private class LinkedListNode {
+        T item;
+        LinkedListNode prev;
+        LinkedListNode next;
+
+        LinkedListNode (T item, LinkedListNode prev, LinkedListNode next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+    private LinkedListNode sentinel;
+    private int size;
+
+    // 构造函数，构造整个LinkedListDeque
+    public LinkedListDeque() {
+        sentinel = new LinkedListNode(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+
+        size = 0;
+    }
+
+    public void addFirst(T item) {
+        LinkedListNode newNode = new LinkedListNode(item, sentinel, sentinel.next);
+
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+
+        size++;
+    }
+
+    public void addLast(T item) {
+        LinkedListNode newNode = new LinkedListNode(item, sentinel.prev, sentinel);
+
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    // Prints the items in the deque from first to last, separated by a space.
+    public void printDeque() {
+        LinkedListNode tempNode = sentinel.next;
+        for (int i = 0; i < size; i++) {
+            System.out.print(tempNode.item + " ");
+            // update
+            tempNode = tempNode.next;
+        }
+        System.out.println();
+    }
+
+    // Removes and returns the item at the front of the deque. If no such item exists, returns null.
+    public T removeFirst() {
+        // check whether the Deque is null
+        if (size == 0) {
+            return null;
+        }
+        else {
+            LinkedListNode tempNode = sentinel.next;
+
+            sentinel.next = tempNode.next;
+            tempNode.next.prev = sentinel;
+
+            size --;
+            return tempNode.item;
+        }
+    }
+
+    public T removeLast() {
+        // check whether the Deque is null
+        if (size == 0) {
+            return null;
+        }
+        else {
+            LinkedListNode tempNode = sentinel.prev;
+
+            sentinel.prev= tempNode.prev;
+            tempNode.prev.next= sentinel;
+
+            size--;
+            return tempNode.item;
+        }
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+
+        LinkedListNode tempNode = sentinel.next;
+
+        for (int i = 0; i < index; i++) {
+            tempNode = tempNode.next;
+        }
+
+        return tempNode.item;
+    }
+
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return getRecursiveHelper(sentinel.next, index);
+    }
+
+    private T getRecursiveHelper(LinkedListNode node, int index) {
+        if (index == 0) {
+            return node.item;
+        }
+        return getRecursiveHelper(node.next, index - 1);
+    }
+}
